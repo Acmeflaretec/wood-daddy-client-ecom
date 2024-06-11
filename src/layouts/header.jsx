fd20964078580abf53eec38147fb1852c1f89b1f
shahil-mohammed-ms <115145954+shahil-mohammed-ms@users.noreply.github.com>
2024-06-12 02:17:52 +0530

    update

--- src/layouts/header.jsx
+++ src/layouts/header.jsx
@@ -236,31 +236,99 @@ const logout = () => {
 
       <div className="mob-search-bar">
 
-<form   style={{ display: 'flex' }}>
+      <form   style={{ display: 'flex' }} onSubmit={handleSearch} >
 
-        <SearchIcon />
+<SearchIcon className='headicons' onClick={handleSearch} />
 
-      <input
-        type="text"
-         
-        placeholder="Search..."
-        style={{
-          padding: '8px',
-          borderRadius: '4px',
-          border: 'none',
-          outline: 'none',
-         
-        }}
-      />
-    
-    </form>
+<input
+type="text"
+ value={searchItem}
+ onChange={(e)=>setSearchItem(e.target.value)}
+placeholder="Search..."
+style={{
+  padding: '8px',
+  borderRadius: '4px',
+  border: 'none',
+  outline: 'none',
+ 
+}}
+/>
+
+</form>
 
 </div>
 <div className="mob-grp-bar">
 <NotificationsIcon className='headicons' />
 <FavoriteBorderIcon className='headicons' onClick={()=>navigate('/wishlist')} />
 <ShoppingBasketIcon className='headicons' />
-
+<Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
+         
+         <Tooltip title="Account settings">
+           <IconButton
+             onClick={handleClick}
+             size="small"
+             sx={{ ml: 2 }}
+             aria-controls={open2 ? 'account-menu' : undefined}
+             aria-haspopup="true"
+             aria-expanded={open2 ? 'true' : undefined}
+           >
+             <Avatar sx={{ width: 32, height: 32 }}>M</Avatar>
+           </IconButton>
+         </Tooltip>
+       </Box>
+       <Menu
+         anchorEl={anchorEl}
+         id="account-menu"
+         open={open2}
+         onClose={handleClose}
+         onClick={handleClose}
+         PaperProps={{
+           elevation: 0,
+           sx: {
+             overflow: 'visible',
+             filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+             mt: 1.5,
+             '& .MuiAvatar-root': {
+               width: 32,
+               height: 32,
+               ml: -0.5,
+               mr: 1,
+             },
+             '&::before': {
+               content: '""',
+               display: 'block',
+               position: 'absolute',
+               top: 0,
+               right: 14,
+               width: 10,
+               height: 10,
+               bgcolor: 'background.paper',
+               transform: 'translateY(-50%) rotate(45deg)',
+               zIndex: 0,
+             },
+           },
+         }}
+         transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+         anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+       >
+         <MenuItem onClick={handleClose}>
+           <Avatar /> Profile
+         </MenuItem>
+         <Divider />
+       
+         <MenuItem onClick={handleClose}>
+           <ListItemIcon>
+             <Settings fontSize="small" />
+           </ListItemIcon>
+           Settings
+         </MenuItem>
+         <MenuItem onClick={logout}>
+           <ListItemIcon>
+             <Logout fontSize="small" />
+           </ListItemIcon>
+           Logout
+         </MenuItem>
+       </Menu>
 <div>
       <IconButton
         onClick={toggleDrawer(true)}
